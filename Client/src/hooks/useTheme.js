474c56a5cfe957d107eaed07d9e5c96ac265565d
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-07 12:33:56 +0530

    made theme switch

--- Client/src/hooks/useTheme.js
+++ Client/src/hooks/useTheme.js
@@ -1,29 +1,28 @@
 import { useEffect, useState } from "react";
 
 export function useTheme() {
-  const root = document.getElementById("root");
   const preferred = window.matchMedia("(prefers-color-scheme: dark)");
   const [theme, setTheme] = useState("");
 
   useEffect(() => {
     const stored = localStorage.getItem("theme");
-    if (stored) {
-      setTheme(stored);
-      root.dataset["theme"] = stored;
-    } else {
+    if (stored) setTheme(stored);
+    else {
       const systemTheme = preferred.matches ? "dark" : "light";
       setTheme(systemTheme);
       localStorage.setItem("theme", systemTheme);
-      root.dataset["theme"] = systemTheme;
     }
-    console.log(root);
   }, []);
+
+  useEffect(() => {
+    const html = document.querySelector("html");
+    html.dataset["theme"] = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   const themeToggle = () => {
     const newTheme = theme === "dark" ? "light" : "dark";
     setTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
-    root.dataset["theme"] = newTheme;
   };
 
   return [theme, themeToggle];
