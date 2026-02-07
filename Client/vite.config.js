18c39b327c8a194c4da650818f14cfb6cd729877
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-07 11:51:30 +0530

    update paths

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
       "@ui": path.resolve(__dirname, "./src/ui"),
       "@pages": path.resolve(__dirname, "./src/pages"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@styles": path.resolve(__dirname, "./src/styles"),
       "@components": path.resolve(__dirname, "./src/components"),
     },
