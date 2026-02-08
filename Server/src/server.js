d9f77684a2804f4eecfb35a54ebe36802026bbc0
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-08 18:45:39 +0530

    added cors in backend

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,9 +1,16 @@
 import "dotenv/config";
+import cors from "cors";
 import express from "express";
 import imageRoutes from "./routes/imageRoutes.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use("/api", imageRoutes);
 
