1565da7dbbc30d846ebf1300fe0947b7d56e2df0
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-08 18:45:07 +0530

    added fetching logic to homepage

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -1,7 +1,48 @@
 import style from "@styles/pages/Home.module.css";
+import { useEffect, useState } from "react";
+import api from "@/lib/api.js";
 
-function Home() {
-  return <section className={style.grid}></section>;
-}
+export default () => {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState("");
+  const [length, setLength] = useState(0);
 
-export default Home;
+  const uniqueId = new Set();
+
+  const fetchData = async (query = "") => {
+    try {
+      const response =
+        query === "" ? await api.get(`/api`) : await api.get(`/api/${query}`);
+      const data = response.data;
+      setImages(data);
+    } catch (er) {
+      console.log("Error while fetching", er);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(query);
+  }, [query]);
+
+  useEffect(() => {
+    if (query !== "") setLength(images[0]);
+    console.log(images);
+    images.map((image) => {
+      if (image === undefined) return null;
+      const { id } = image;
+      uniqueId.add(id);
+      console.log(uniqueId);
+    });
+  }, [images]);
+
+  return (
+    <section className={style.HomePage}>
+      <button onClick={() => setQuery("cats")}>CLICK ME</button>
+      <div className={style.Grid}>
+        {images.map((image, index) => {
+          if (query !== "" && index === 0) return null;
+        })}
+      </div>
+    </section>
+  );
+};
